189f45dd3f297c480da554b1c467f7b47c0a743f
Sy Le <sle@salesforce.com>
2017-01-16 11:52:23 -0800

    support salesforce aura template

---
+++ make-salesforce-aura-all-component.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+//includes
+require('./index')([
+    ['templates/salesforce-aura/cmp.template', '.cmp'],
+    ['templates/salesforce-aura/controller.template', 'Controller.js'],
+    ['templates/salesforce-aura/css.template', '.css'],
+    ['templates/salesforce-aura/helper.template', 'Helper.js'],
+    ['templates/salesforce-aura/renderer.template', 'Renderer.js'],
+    ['templates/salesforce-aura/test.template', 'Test.js'],
+]);
