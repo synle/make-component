fbd5924d0ee85aac65b9d7be4648526974fb56a5
Sy Le <lenguyensy@gmail.com>
2020-02-11 17:11:51 -0800

    Update make-react-component.js

--- make-react-component.js
+++ make-react-component.js
@@ -3,7 +3,7 @@
 require('./index').processWithPrompt(
     'Enter React Component name',
     [
-        ['templates/react/component', '.js', true],
-        ['templates/react/style', '.scss', true],
+        ['templates/react/component', '.js'],
+        ['templates/react/style', '.scss'],
     ]
 );
