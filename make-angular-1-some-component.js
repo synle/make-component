ec5662ac43c3dc37410b6a37d4e58dd7bb75c263
Sy Le <lenguyensY@gmail.com>
2020-11-10 10:26:42 -0800

    clean up unused componnet

--- make-angular-1-some-component.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-//includes
-require('./index').process([
-    ['templates/angular-1/index_some', 'index.js', true],
-    ['templates/angular-1/ctrl', 'Ctrl.js'],
-    ['templates/angular-1/drtv', 'Drtv.js'],
-    ['templates/angular-1/html', '.html'],
-]);
