fda9bd18d0ac0aa562afac46d753fd85d5af7d52
Sy Le <syle@linkedin.com>
2020-11-10 10:16:59 -0800

    minor bug fixed the template

--- make-react-typescript-components.js
+++ make-react-typescript-components.js
@@ -1,9 +1,7 @@
-#!/usr/bin/env node
-
 require('./index').processWithPrompt(
     'Enter React Component name',
     [
         ['templates/react-v2-typescript/component', 'index.tsx', true],
-        ['templates/react-v2-typescript/style', 'index.scss', true],
+        ['templates/react-v2-typescript/style', 'index.module.scss', true],
     ]
 );
