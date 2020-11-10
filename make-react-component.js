5fba0f80c0ad2bf35dcbf731db713a70d687e99d
Sy Le <lenguyensY@gmail.com>
2020-11-10 10:07:36 -0800

    minor path refactor

--- make-react-component.js
+++ make-react-component.js
@@ -3,7 +3,7 @@
 require('./index').processWithPrompt(
     'Enter React Component name',
     [
-        ['templates/react/component', '.js'],
-        ['templates/react/style', '.scss'],
+        ['templates/react-v1/component', '.js'],
+        ['templates/react-v1/style', '.scss'],
     ]
 );
