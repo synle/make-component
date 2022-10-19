e2044e245405a809dc5f7211b86b25e3c0839559
Sy Le <lenguyensy@gmail.com>
2022-10-18 20:24:00 -0700

    run formatter script

--- make-react-typescript-components.js
+++ make-react-typescript-components.js
@@ -2,6 +2,6 @@
 
 //includes
 require('./index').process([
-    ['templates/react-v2-typescript/component', 'index.tsx', true],
-    ['templates/react-v2-typescript/style', 'index.module.scss', true],
+  ['templates/react-v2-typescript/component', 'index.tsx', true],
+  ['templates/react-v2-typescript/style', 'index.module.scss', true],
 ]);
